f05c20096e6f4d37041aefed43931f9a6c3c95a5
Garlic Fiend <suzannaakins@gmail.com>
2020-08-22 15:22:11 -0700

    getRandom function implemented so there's DRY on getting random characters, consolidated confirm variables, created guaranteed chars array

--- Develop/pseudocode.js
+++ Develop/pseudocode.js
@@ -1,12 +1,13 @@
 //define arrays (lowercase, etc.), 4 variable with all the characters ARRAYS, then ASK user for length (prompt), then save that in a variable so you can reference later
--
-//confirm if they want numbers, uppcase, lwrcase, spcl char
-//store response in the variable (T or F)
-// create variable containig password characters (final password) - will be empty string
-// define variable to add random stuff
+
+//confirm if they want numbers, uppcase, lwrcase, spcl char - X
+//store response in the variable (T or F) - X
+//make sure at least ONE of them was confirmed true - X
+// create variable containng password characters (final password) - will be empty string
+// have an array of guaranteed characters so that you can get at least one of each type of character - based on what user said
 //another array (possible characters)containing ALL possible characters that can go in the password - depends on what the user said
 //now generate random numbers - take options
-//make sure at least ONE of them was confirmed true
+// put the guaranteed and the possible ones together
 // if user chose THIS, then all of that option goes into possible char array
 
 if (lowerCase) {
--- Develop/script.js
+++ Develop/script.js
@@ -5,77 +5,98 @@ var generateBtn = document.querySelector("#generate");
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialChar = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
-  "~", "*", "?", ":", "\"", "\\"];
+var specialChar = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+  "~", "*", "?", ":", "\""];
 
-//ask if user wants lowercase letters 
-var lowerCaseConfirm = window.confirm("Would you like lowercase letters included in your password?");
-if (lowerCaseConfirm) {
-  ////push all lowercase to array
-  concat //concatinate 
-  allpossiblecharactersarray.concat(lowerCase)
-  //generate RANDOM lowercase - function
-  //add THAT random lowercase to password RESULT(string)
-}
-else {
-  //don't include lowercase
-}
+var getRandom = function (x) {
+  var random = x[Math.floor(Math.random() * x.length)]
+  console.log(random);
+  return random;
 }
 
-//ask if user want uppercase letters
-var upperCaseConfirm = window.confirm("Would you like uppercase letters included in your password?");
-if (upperCaseConfirm) {
-  //generate random uppercase letters for password
-}
-else {
-  //don't include uppercase
-}
+var getLower = getRandom(lowerCase)
+console.log(getLower)
 
-//ask if user wants numbers
-var numberConfirm = window.confirm("Would you like numbers included in your password?");
-if (numberConfirm) {
-  //generate random numbers (Between 0 and 9) for password
-  var randomNumber = Math.floor(Math.random() * 10);
-}
-else {
-  //don't include numbers
-}
+var getUpper = getRandom(upperCase)
+console.log(getUpper)
 
-//ask if user wants special characters
-var specialConfirm = window.confirm("Would you like special characters included in your password?");
-if (specialConfirm) {
-  //generate random special characters for password
-}
-else {
-  //don't include special characters
-}
+var getNumber = getRandom(number)
+console.log(getNumber)
 
-
+var getSpecial = getRandom(specialChar)
+console.log(getSpecial)
 
 // write Generate Password Function
 function generatePassword() {
   //prompt user to enter length between 8 and 128
-  var length = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-  while (length === "" || length === null || length < 8 || length > 128) {
-    window.prompt("That's not a valid option. Please choose a number between 8 and 128.")
+  var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+  while (passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128) {
+    // add passwordLenth = NOT A NUMBER
+    console.log(passwordLength);
+    passwordLength = prompt("That's not a valid option. Please choose a number between 8 and 128.")
   }
-  console.log("Password length will be " + length + "characters long.")
+  console.log("Password length will be " + passwordLength + "characters long.")
+
+  //ask if user wants lowercase letters 
+  var lowerCaseConfirm = confirm("Would you like lowercase letters included in your password?");
+  var upperCaseConfirm = confirm("Would you like uppercase letters included in your password?");
+  var numberConfirm = confirm("Would you like numbers included in your password?");
+  var specialConfirm = confirm("Would you like special characters included in your password?");
+
+  while ((!lowerCaseConfirm) && (!upperCaseConfirm) && (!numberConfirm) && (!specialConfirm)) {
+    alert("You need to choose at least one option. Thank you!")
+    lowerCaseConfirm = confirm("Would you like lowercase letters included in your password?");
+    upperCaseConfirm = confirm("Would you like uppercase letters included in your password?");
+    numberConfirm = confirm("Would you like numbers included in your password?");
+    specialConfirm = confirm("Would you like special characters included in your password?");
+  }
+
+  var guaranteedChars = [];
+  var possibleChars = [];
+  if (lowerCaseConfirm) {
+    var guaranteeLower = getRandom(lowerCase);
+    guaranteedChars.push(guaranteeLower);
+  }
+
+  //ask if user want uppercase letters
+  if (upperCaseConfirm) {
+    var guaranteeUpper = getRandom(upperCase);
+    guaranteedChars.push(guaranteeUpper);
+  }
+
+  //ask if user wants numbers
+  if (numberConfirm) {
+    var guaranteeNumber = getRandom(number)
+    guaranteedChars.push(guaranteeNumber);
+  }
+
+  //ask if user wants special characters
+  if (specialConfirm) {
+    var guaranteeSpecial = getRandom(specialChar)
+    guaranteedChars.push(guaranteeSpecial);
+  }
+
+
+  var password = []
+  // for loop to grab the rest of the characters from possibleChars array, randomly
+
+  // to add them together you do guaranteedChars.concat(possibleChars);
+  // join array, look up array join for javascript
+  return password;
 };
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-};
-
   //based on answers, write functions to retrieve random values for criteria by using Math.random
 
   //display alert the password
-}
+  //}
 
-var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
+  passwordText.value = password;
+};
 
 // Add event listener to generate button
-debugger;
 generateBtn.addEventListener("click", writePassword);

7960676f73ab0fbc04b4ba64aa3c6d4b7eac60cd
Garlic Fiend <suzannaakins@gmail.com>
2020-08-23 14:17:38 -0700

    added for loop to generate password, and made sure user enters a NUMBER for password length

--- Develop/script.js
+++ Develop/script.js
@@ -2,40 +2,46 @@
 var generateBtn = document.querySelector("#generate");
 
 // Assignment code here
+
+//array for numbers 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//array for lowercase letters
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//array for uppcase letters
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//.array for special characters
 var specialChar = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
   "~", "*", "?", ":", "\""];
 
-var getRandom = function (x) {
+// //array for ALL possible characters
+// var possible = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+//   "~", "*", "?", ":", "\""];
+
+//function to get a random item from an array
+var getRandom = function(x) {
   var random = x[Math.floor(Math.random() * x.length)]
-  console.log(random);
   return random;
 }
 
 var getLower = getRandom(lowerCase)
-console.log(getLower)
 
 var getUpper = getRandom(upperCase)
-console.log(getUpper)
 
 var getNumber = getRandom(number)
-console.log(getNumber)
 
 var getSpecial = getRandom(specialChar)
-console.log(getSpecial)
 
 // write Generate Password Function
 function generatePassword() {
   //prompt user to enter length between 8 and 128
   var passwordLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-  while (passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128) {
+  passwordLength = parseInt(passwordLength)
+  while (passwordLength === "" || passwordLength === null || passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     // add passwordLenth = NOT A NUMBER
     console.log(passwordLength);
     passwordLength = prompt("That's not a valid option. Please choose a number between 8 and 128.")
   }
-  console.log("Password length will be " + passwordLength + "characters long.")
+  console.log("Password length will be " + passwordLength + " characters long.")
 
   //ask if user wants lowercase letters 
   var lowerCaseConfirm = confirm("Would you like lowercase letters included in your password?");
@@ -45,44 +51,58 @@ function generatePassword() {
 
   while ((!lowerCaseConfirm) && (!upperCaseConfirm) && (!numberConfirm) && (!specialConfirm)) {
     alert("You need to choose at least one option. Thank you!")
-    lowerCaseConfirm = confirm("Would you like lowercase letters included in your password?");
-    upperCaseConfirm = confirm("Would you like uppercase letters included in your password?");
-    numberConfirm = confirm("Would you like numbers included in your password?");
-    specialConfirm = confirm("Would you like special characters included in your password?");
+    var lowerCaseConfirm = confirm("Would you like lowercase letters included in your password?");
+    var upperCaseConfirm = confirm("Would you like uppercase letters included in your password?");
+    var numberConfirm = confirm("Would you like numbers included in your password?");
+    var specialConfirm = confirm("Would you like special characters included in your password?");
   }
 
+  //array of GUARANTEED characters (must include these based on user's answers to confirm questions)
   var guaranteedChars = [];
+
   var possibleChars = [];
+
   if (lowerCaseConfirm) {
-    var guaranteeLower = getRandom(lowerCase);
-    guaranteedChars.push(guaranteeLower);
+    possibleChars = possibleChars.concat(lowerCase);
+    guaranteedChars.push(getLower);
   }
 
   //ask if user want uppercase letters
   if (upperCaseConfirm) {
-    var guaranteeUpper = getRandom(upperCase);
-    guaranteedChars.push(guaranteeUpper);
+    possibleChars = possibleChars.concat(upperCase);
+    guaranteedChars.push(getUpper);
   }
 
   //ask if user wants numbers
   if (numberConfirm) {
-    var guaranteeNumber = getRandom(number)
-    guaranteedChars.push(guaranteeNumber);
+    possibleChars = possibleChars.concat(number);
+    guaranteedChars.push(getNumber);
   }
 
   //ask if user wants special characters
   if (specialConfirm) {
-    var guaranteeSpecial = getRandom(specialChar)
-    guaranteedChars.push(guaranteeSpecial);
+    possibleChars = possibleChars.concat(specialChar);
+    guaranteedChars.push(getSpecial);
   }
 
+  console.log(possibleChars)
 
-  var password = []
+  //array of guaranteed AND all possible characters
+  var password = guaranteedChars;
+  
+  var totalLength = passwordLength - guaranteedChars.length
   // for loop to grab the rest of the characters from possibleChars array, randomly
+  for (var j = 0; j < totalLength; j++) {
+    password.push(getRandom(possibleChars))
+  }
+  var finalPassword = ""
+  for (var i=0; i < password.length; i++) {
+    finalPassword = finalPassword + password[i]
+  }
 
   // to add them together you do guaranteedChars.concat(possibleChars);
   // join array, look up array join for javascript
-  return password;
+  return finalPassword;
 };
 
 // Write password to the #password input
